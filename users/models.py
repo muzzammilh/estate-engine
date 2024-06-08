c6ed98145f190e8a739e69cd75ae7b43d7eed743
Muzzammil Hussain <muzzammilz@gmail.com>
2024-06-08 17:14:26 +0500

    initial setup for the django and user model

---
+++ users/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+from .managers import UserManager
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=150, blank=True, null=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
