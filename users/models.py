818586ffd5cd975d2dca16625bee39dd1b8f1670
Muzzammil Hussain <muzzammilz@gmail.com>
2024-06-09 15:23:29 +0500

    Added flake8 and Github Action (#2)
    * Added flake8 and Github Action
    
    * Fixed the issues reported by Flake8

--- users/models.py
+++ users/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import AbstractUser
 
 from .managers import UserManager
 
+
 class User(AbstractUser):
     email = models.EmailField(unique=True)
     username = models.CharField(max_length=150, blank=True, null=True)
