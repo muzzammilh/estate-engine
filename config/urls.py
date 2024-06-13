fd0d7066a34d687c3c0fe2d00bf68a1166409d7e
Muzzammil Hussain <muzzammilz@gmail.com>
2024-06-13 14:44:27 +0500

    Added logging

--- config/urls.py
+++ config/urls.py
@@ -17,10 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from .views import HomePageView
+from .views import HomePageView, LoggingExampleView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('', HomePageView.as_view(), name='home'),
+    path('logging/', LoggingExampleView, name='home'),
 ]
