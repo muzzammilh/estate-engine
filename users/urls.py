a712b84ee160b4b8c5cf351b7defe8e2d4668031
Abdullah <abdullaharshed956@gmail.com>
2024-06-20 18:33:30 +0500

    refactor landlord to owner

--- users/urls.py
+++ users/urls.py
@@ -3,9 +3,9 @@ from django.views.generic import TemplateView
 
 from .views import (CustomPasswordResetCompleteView,
                     CustomPasswordResetConfirmView, CustomPasswordResetView,
-                    LandlordDashboardView, PasswordChangeView,
-                    ProfileUpdateView, TenantDashboardView, UserLoginView,
-                    UserLogoutView, UserRegisterView)
+                    OwnerDashboardView, PasswordChangeView, ProfileUpdateView,
+                    TenantDashboardView, UserLoginView, UserLogoutView,
+                    UserRegisterView)
 
 urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='register'),
@@ -15,7 +15,7 @@ urlpatterns = [
     path('password-reset/done/', TemplateView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', CustomPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done/', CustomPasswordResetCompleteView.as_view(), name='password_reset_complete'),
-    path('landlord-dashboard/', LandlordDashboardView.as_view(), name='landlord_dashboard'),
+    path('owner-dashboard/', OwnerDashboardView.as_view(), name='owner_dashboard'),
     path('tenant-dashboard/', TenantDashboardView.as_view(), name='tenant_dashboard'),
     path('profile-update/', ProfileUpdateView.as_view(), name='profile_update'),
     path('password-update/', PasswordChangeView.as_view(), name='password_change'),
